0450b2703971ba8e702370915f4061cc28bb4ad2
Sheezylodhi <muhammadshahzaiblodhi@gmail.com>
2025-10-26 05:06:15 +0500

    ..

--- src/lib/scraper.js
+++ src/lib/scraper.js
@@ -31,7 +31,7 @@ export async function scrapeEbayCars(
 
 const browser = await puppeteer.launch({
   headless: true,
-  executablePath: "/snap/bin/chromium", // VPS pe installed Chromium
+  executablePath: "/usr/bin/chromium-browser",
   args: [
     "--no-sandbox",
     "--disable-setuid-sandbox",
