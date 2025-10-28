7d941ee413970a667ee8b168364ce62c2bcf4147
Sheezylodhi <muhammadshahzaiblodhi@gmail.com>
2025-10-29 00:28:11 +0500

    ,,,,

--- src/lib/scraper.js
+++ src/lib/scraper.js
@@ -72,8 +72,8 @@ export async function scrapeEbayCars(
   console.log("✅ Scrape params:", { searchUrl, maxPages, keyword, from, to, siteName });
 
   const browser = await puppeteer.launch({
-
-    headless: false,
+ executablePath: '/usr/bin/google-chrome-stable',
+    headless: true,
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
